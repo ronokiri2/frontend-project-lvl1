20c22c584d44dced1dccf990ff6caf89d67a022c
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-03-30 19:59:49 +0300

    added codeclimate, eslintm github actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import returnName from '../src/cli.js';
 
-import returnName from '../src/cli.js'
+console.log('Welcome to the Brain Games!');
 
 returnName();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from 'readline-sync';	
+import readlineSync from 'readline-sync';
 
-const returnName = () => { 
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}`);
-}
+const returnName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
 
 export default returnName;
